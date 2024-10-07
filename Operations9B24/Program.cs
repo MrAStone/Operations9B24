fb6474f5eac48acd1b05de3e8a2f9bec52829331
Al Stone <astone@heckgrammar.co.uk>
2024-10-07 10:24:46 +0100

    Relational

--- Operations9B24/Program.cs
+++ Operations9B24/Program.cs
@@ -45,6 +45,52 @@
             num2 = 2;
             num3 = num1 / num2;
             Console.WriteLine(num1 / num2);
+            Console.WriteLine(9/2); // 9 and 2 are integers
+            Console.WriteLine(9/2.0); // 9 is an integer 2.0 is a double
+            Console.WriteLine(9.0/2);
+
+            // remainder
+            Console.WriteLine(7%2); // 7 MOD 2 = 1
+
+            Console.WriteLine(Math.Pow(2,8));
+            Console.WriteLine(Math.Pow(2,-8));
+            Console.WriteLine(Math.Sqrt(81));
+
+            // Relational Operators
+            int a = 3;
+            // equal to
+            if (a == 3)
+            {
+                Console.WriteLine("It is 3");
+            }
+            // not equal to
+            if(a != 3)
+            {
+                Console.WriteLine("It is not 3");
+            }
+            // less than
+            if (a < 3)
+            {
+                Console.WriteLine("less than 3");
+            }
+            // greater than
+            if (a > 3)
+            {
+                Console.WriteLine("greater than 3");
+            }
+
+            // less than or equal to
+            if(a <= 3)
+            {
+                Console.WriteLine("less than or equal to 3");
+            }
+            // greater than or equal to
+            if(a >= 3)
+            {
+                Console.WriteLine("greater than or equal to 3");
+            }
+
+
 
 
         }
